c96a86d43f41b1f6b3a7eae725a777b93b51fe97
Guangjian Bao <guangjianbao@gmail.com>
2022-05-08 11:33:09 -0400

    add books from my api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "./components/navbar";
+import axios from "axios";
 
 function App() {
+
+  const [bookurl, setBookurl] = useState(null);
+  let getBooks = async () => {
+    let booksurl = [];
+    let books = await axios("https://myfavaritebook.herokuapp.com/books");
+
+
+    console.log(books.data[0].image);
+
+    booksurl.push(books.data[0].image)
+    booksurl.push(books.data[1].image)
+    booksurl.push(books.data[2].image)
+    booksurl.push(books.data[3].image)
+    setBookurl(booksurl);
+    console.log(booksurl)
+    console.log(bookurl)
+  };
+
+  // make api call
+  useEffect(() => {
+    getBooks();
+  }, []);
   return (
     <div>
       <Navbar />
+
+      <div><img className="img" src=""></img></div>
+
+
     </div>
+
 
   );
 }
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,13 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
+import "../navbar.css";
+
 
 
 function Navbar() {
+  const [active, setActive] = useState("nav__menu");
+  const [icon, setIcon] = useState("nav__toggler");
+  const navToggle = () => {
+    if (active === "nav__menu") {
+      setActive("nav__menu nav__active");
+    } else setActive("nav__menu");
+
+    // Icon Toggler
+    if (icon === "nav__toggler") {
+      setIcon("nav__toggler toggle");
+    } else setIcon("nav__toggler");
+  };
 
   return (
-    <div>
-      <h2>Hello world!</h2>
-    </div>
+    <nav className="nav">
+      <a href="#" className="nav__brand">
+        guangjian's bookshop
+      </a>
+      <ul className={active}>
+        <li className="nav__item">
+          <a href="#" className="nav__link">
+            Home
+          </a>
+        </li>
+        <li className="nav__item">
+          {/* <a href="#" className="nav__link">
+            Food
+          </a> */}
+          <button onClick={() => { console.log('hello!') }}>books</button>
+        </li>
+        <li className="nav__item">
+          <a href="#" className="nav__link">
+            About
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="#" className="nav__link">
+            Portfolio
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="#" className="nav__link">
+            Skills
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="#" className="nav__link">
+            Contact
+          </a>
+        </li>
+      </ul>
+      <div onClick={navToggle} className={icon}>
+        <div className="line1"></div>
+        <div className="line2"></div>
+        <div className="line3"></div>
+      </div>
+    </nav>
   );
 }
 
-export default Navbar 
+export default Navbar;
